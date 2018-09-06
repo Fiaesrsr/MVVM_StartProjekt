94a7102e3651e0a9d6ab74a57334f2b2b21503f2
Sebastian Rogacki <S.Rogacki2012@gmail.com>
2018-09-06 13:01:09 +0200

    Fertiges MVVM-Konzept

---
+++ MVVM_StartProjekt/Models/Example_Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using Prism.Mvvm;
+using MVVM_StartProjekt.ExtensionMethods;
+
+namespace MVVM_StartProjekt.Models
+{
+    class Example_Model : BindableBase
+    {
+    }
+}
---
+++ MVVM_StartProjekt/ViewModels/ShellViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MVVM_StartProjekt.Models;
+using Prism.Mvvm;
+using MVVM_StartProjekt;
+using MVVM_StartProjekt.ExtensionMethods;
+using System.Data;
+using System.Collections.Generic;
+
+namespace MVVM_StartProjekt.ViewModels
+{
+    class ShellViewModel : BindableBase
+    {
+
+
+        /// <summary>
+        /// DataBinding für MainWindow Titel
+        /// </summary>
+        public string Title { get; set; } = "MVVM - Konzept";
+        
+
+    }
+}
