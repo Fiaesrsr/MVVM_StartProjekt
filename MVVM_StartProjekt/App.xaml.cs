ed84b515fcf8e1c966b1d3c91d978b946af33e33
Sebastian Rogacki <S.Rogacki2012@gmail.com>
2018-09-06 12:30:10 +0200

    Projektdateien hinzufügen.

---
+++ MVVM_StartProjekt/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MVVM_StartProjekt.App_Start;
+using System.Windows;
+
+namespace MVVM_StartProjekt
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            new Bootstrapper().Run();
+        }
+    }
+}
---
+++ MVVM_StartProjekt/App_Start/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+using System.Windows;
+
+namespace MVVM_StartProjekt.App_Start
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Views.Shell>();
+        }
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow?.Show();
+        }
+    }
+}
